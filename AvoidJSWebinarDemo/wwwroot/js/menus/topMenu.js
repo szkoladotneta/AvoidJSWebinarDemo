0d107079f0618424053746c2c54e38871219e31a
szkoladotneta <kajetan@szkoladotneta.pl>
2022-04-06 13:21:23 +0200

    Demo Code

---
+++ AvoidJSWebinarDemo/wwwroot/js/menus/topMenu.js
@@ -0,0 +1,8 @@
+﻿var switchMenu = function () {
+    var sidebarSize = $('body').attr('data-sidebar-size');
+    if (sidebarSize === 'condensed') {
+        $('body').attr('data-sidebar-size', 'default');
+    } else {
+        $('body').attr('data-sidebar-size', 'condensed');
+    }
+}
---
+++ AvoidJSWebinarDemo/wwwroot/js/pages/availabilityMap.js
@@ -0,0 +1,13 @@
+﻿var setMap = function (availability) {
+    var map = new Datamap({
+        element: document.getElementById('map'),
+        fills: {
+            'EXCLUDED': '#306596',
+            'SOLDEXCLUSIVE': '#0fa0fa',
+            'SOLDNONEXCLUSIVE': '#bada55',
+            defaultFill: '#dddddd'
+        },
+        'data': availability,
+    });
+    map.legend();
+}
